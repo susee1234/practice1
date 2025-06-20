36826587e20d339366bd75166916032eb0db5ad5
Susee Laksman B S <suseelaksman@Susees-MacBook-Pro.local>
2025-06-20 11:08:09 +0530

    both scenarios

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 #this is my first git project june 20.
+
+
+#scenario 1:
+# create githun repo
+#clone it
+#add files
+#commit
+#git push
+
+
+
+#scenario 2:
+#starting with git locally
+#create a project structure locally
+#git init
+#git remote add origin <remote-url>
+#git add .
+#git commit
+#git push
